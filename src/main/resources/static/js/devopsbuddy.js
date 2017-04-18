0948ed5658c7798070e796305397ca2f8f9ff760
Carren.Dsouza <onlinecarren@gmail.com>
2017-04-18 21:18:40 +0400

    jquery

---
+++ src/main/resources/static/js/devopsbuddy.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Carren.Dsouza on 18/04/2017.
+ */
+$(document).ready(main);
+
+function main(){
+
+    ${'.btn-collapse'}.click(function(e){
+        e.preventDefault();
+        var $this= $(this);
+        var $collapse= $this.closest('.collapse-group').find('.collapse');
+        $collapse.collapse('toggle');
+    });
+
+}
