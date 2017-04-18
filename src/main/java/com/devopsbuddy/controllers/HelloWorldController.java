b6db6ca03876413507252f017e98f858a9af83e9
Carren.Dsouza <onlinecarren@gmail.com>
2017-04-18 18:06:57 +0400

    Intial COmmint

---
+++ src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.devopsbuddy.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Carren.Dsouza on 18/04/2017.
+ */
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String sayHello(){
+        return "index";
+    }
+}
